ba6345618f14e1b565727c374bf58c7ac82220dc
DevMahlatsi <j.mahlatsi@outlook.com>
2026-01-01 22:32:17 +0200

    Think we might be done for the day. It looks perfect for now but need to work on the api calls

--- Components/Hero/Hero.tsx
+++ Components/Hero/Hero.tsx
@@ -1,10 +1,31 @@
 export default function Hero() {
   return(
     <>
-      <div className="img-container h-60 border-8 border-amber-950 w-full overflow-hidden" > 
+      <div className="img-container h-55 mt-2 w-full overflow-hidden relative" > 
         <img src="/wp5501421-wide-anime-wallpaper naruto.jpg" 
         alt="Naruto wide anime wallpaper"
-        className="hero-img w-full h-full object-cover object-center" />
+        className="hero-img 
+        w-full h-full 
+        object-cover 
+        object-center
+        
+        " />
+        <div 
+        className="
+          overlay
+          absolute
+          inset-0
+          flex
+          flex-col
+          justify-center
+          items-center
+          text-white
+          text-center
+          bg-[rgba(0,0,0,0.35)]
+          ">
+          <h1 className="text-2xl m-0">Merchbay</h1>
+          <p>Discover anime-inspired merch</p>
+        </div>
       </div>
     </>
     
