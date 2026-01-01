c04338962bf5a582aacaf59eb5ae054ab3b4661e
DevMahlatsi <j.mahlatsi@outlook.com>
2026-01-01 21:50:31 +0200

    Image is now inserted in the hero section. Next is to put a CTA on the image if possible. also remove the brown border dawg

--- Components/Hero/Hero.tsx
+++ Components/Hero/Hero.tsx
@@ -1,5 +1,13 @@
 export default function Hero() {
   return(
-    <img src="/wp5501421-wide-anime-wallpaper naruto.jpg" alt="Naruto wide anime wallpaper" />
+    <>
+      <div className="img-container h-60 border-8 border-amber-950 w-full overflow-hidden" > 
+        <img src="/wp5501421-wide-anime-wallpaper naruto.jpg" 
+        alt="Naruto wide anime wallpaper"
+        className="hero-img w-full h-full object-cover object-center" />
+      </div>
+    </>
+    
+    
   )
 };
