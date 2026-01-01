41366d37573e2054236bbf8cff796b228e0aafbc
DevMahlatsi <j.mahlatsi@outlook.com>
2026-01-01 23:11:30 +0200

    Feature now works for cart. For components that do not exist yet.

--- Components/Navigation Bar/Cart.tsx
+++ Components/Navigation Bar/Cart.tsx
@@ -1,11 +1,27 @@
+'use client';
+import { useState } from "react";
+import Toast from "../Toast/Toast";
+
 export default function cart() {
+
+const [showToast, setShowToast] = useState(false);
+
+const handleComingSoon = () => {
+    setShowToast(true);
+  };
   return(
     <>
       <div className="cart">
-        <img src="/cart-shopping-svgrepo-com.svg" alt="cart-icon" 
+        <img  onClick={handleComingSoon}
+        src="/cart-shopping-svgrepo-com.svg" alt="cart-icon" 
         className="nav-icon nav-icon-lg"
         />
       </div>
+      <Toast
+      message="Cart feature is not available yet!"
+      visible={showToast}
+      onClose={() => setShowToast(false)}
+      />
     </>
   )
 };
