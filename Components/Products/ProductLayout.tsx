d21ff8f1bc49fbef9e2e4e6bf0e0e645b0b2c8e9
DevMahlatsi <j.mahlatsi@outlook.com>
2026-01-02 23:55:20 +0200

    final bug fixes before clocking out done

--- Components/Products/ProductLayout.tsx
+++ Components/Products/ProductLayout.tsx
@@ -13,38 +13,18 @@ export default function ProductsLayout({title}: ProductLayoutProps){
   return(
     <>
     <div className="main-container">
-      <div className="sub-container">
         <div className="product-container mt-2 bg-amber-50  text-blue-950">
           <div className="header">
-            <Title
-              headingTitle={title}
-            />  
-        </div>
+            <Title headingTitle={title}/>  
+          </div>
+        
         {/* to be made it's own component */}
         <div className="products">
           <br /><br />
           Here it will be the categories and this time we hope to put it in a carosel
         </div>
       </div>
-      <div className="sub-container">
-        <div className="product-container mt-2 bg-amber-50  text-blue-950">
-          <div className="header">
-            <Title
-              headingTitle={title}
-            />  
-        </div>
-        {/* to be made it's own component */}
-        <div className="products">
-          <br /><br />
-          Here it will be the categories and this time we hope to put it in a carosel
-        </div>
       </div>
-      
-    </div>
-      
-    </div>
-      
     </>
   )
-
 }
