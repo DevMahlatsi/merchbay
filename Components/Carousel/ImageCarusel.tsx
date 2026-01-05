5044c8c5d9bc7752f21c5162058f8f5b63548b31
DevMahlatsi <j.mahlatsi@outlook.com>
2026-01-05 18:21:11 +0200

    Continuing with image corousel

--- Components/Carousel/ImageCarusel.tsx
+++ Components/Carousel/ImageCarusel.tsx
@@ -1,5 +1,5 @@
 import { ImageCarouselProps } from "@/Types/Merchbay";
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import { ChevronLeft, ChevronRight, Divide } from "lucide-react"
 import { useState } from "react";
 
 export default function ImageCarusel({images, productTitle}: ImageCarouselProps) {
@@ -13,7 +13,27 @@ export default function ImageCarusel({images, productTitle}: ImageCarouselProps)
     prevIndex === 0? images.length - 1 : prevIndex - 1);
   };
   const goToSlide = (index: number) => {
-    setCurrentIndex
-  }
+    setCurrentIndex(index);
+  };
+
+  if(images.length <= 1 ){
+    return(
+      <div className="relative w-full h-full">
+        <img src={images[0]} alt={productTitle} className="w-full h-full object-cover rounded-lg" 
+        />
+      </div>
+    )
+  };
+
+  return (
+    <div className="relative w-full h-full-group">
+      <div className="overflow-hidden rounded-lg">
+        <img 
+          src={images[currentIndex]} 
+          alt={`${productTitle} - Image ${currentIndex + 1}`} 
+          className="w-full h-full object-cover transition-transform duration-300 hover:scale-105" />
+      </div>
+    </div>
+  )
 
 };
