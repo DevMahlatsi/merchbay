526debeaa398b1e24ed09069f9655bd70f06bc0e
DevMahlatsi <j.mahlatsi@outlook.com>
2026-02-19 18:58:37 +0200

    zoro poster added

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 const products = [
   {
     plid: 1,
-    title: "Luffy Wanted Poster",
+    title: "Monkey D Luffy Poster",
     variants: [
       {size: "A2", price: 30.00},
       {size: "A3", price: 20.00},
@@ -17,7 +17,22 @@ const products = [
       main: "/images/anime/one-piece/luffy_wanted_poster_1_main.jpg",
       mockup: "/images/anime/one-piece/luffy_wanted_poster_1_mockup.png"
     }
-  }
+  },
+  {
+    plid: 2,
+    title: "Roronoa Zoro Wanted Poster",
+    variants: [
+      {size: "A2", price: 30.00},
+      {size: "A3", price: 20.00},
+      {size: "A4", price: 10.00},
+      
+    ],
+    images: {
+      main: "/images/anime/one-piece/zoro_wanted_poster_2_main.jpg",
+      mockup: "/images/anime/one-piece/luffy_wanted_poster_1_mockup.png"
+    }
+  },
+  
 ];
 
 export async function GET() {
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "http://192.168.18.52",
-    "http://localhost:3000",
-  ],
+  
 };
 
 export default nextConfig;
