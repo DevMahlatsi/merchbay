dd38ea6c005952dc7becb9c1dfba90639060f8bd
DevMahlatsi <j.mahlatsi@outlook.com>
2026-02-19 19:04:32 +0200

    Usopp poster added

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -32,6 +32,20 @@ const products = [
       mockup: "/images/anime/one-piece/luffy_wanted_poster_1_mockup.png"
     }
   },
+  {
+    plid: 3,
+    title: "God Usopp Poster",
+    variants: [
+      {size: "A2", price: 30.00},
+      {size: "A3", price: 20.00},
+      {size: "A4", price: 10.00},
+      
+    ],
+    images: {
+      main: "/images/anime/one-piece/usopp_wanted_poster_3_main.jpg",
+      mockup: "/images/anime/one-piece/luffy_wanted_poster_3_mockup.png"
+    }
+  },
   
 ];
 
