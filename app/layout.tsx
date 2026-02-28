24d7234eb995741424275808634755a29bc3fa4e
DevMahlatsi <j.mahlatsi@outlook.com>
2026-02-28 23:54:35 +0200

    added analytics monitor to see page visits.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Iceland } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import NavBar from "@/Components/Navigation Bar/NavBar";
 
 const geistSans = Geist({
